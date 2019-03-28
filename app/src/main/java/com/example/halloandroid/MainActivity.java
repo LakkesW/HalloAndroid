7ba3402061a04c1d69cecb4fe41bf1f626120de0
smimsLukas <lukiwichmann@web.de>
2019-03-28 22:38:34 +0100

    Die bisherige Version wurde um eine Reaktion auf das Klicken des Buttons erweitert. Beim Klick des Button wird jetzt angezeigt, wie oft bereits auf den Button gedrückt wurde.

--- app/src/main/java/com/example/halloandroid/MainActivity.java
+++ app/src/main/java/com/example/halloandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.halloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView view;
+    private Button button;
+    private int i=0;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        i++;
+        String string = getResources().getString(R.string.newLayerText);
+        view.setText(String.format(string, i));
     }
 }
