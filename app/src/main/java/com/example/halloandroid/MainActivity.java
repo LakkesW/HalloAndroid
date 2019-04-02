2331ab9c1f0bd232a49242580e51d8920a1897f4
Lukas Wichmann <lukas_wichmann@t-online.de>
2019-04-02 12:49:29 +0200

    Das Spielkonzept wurde so verändert, dass der Benutzer nun aufgefortdert wird den Button 100 mal zu drücken. Hat er dies getan, erscheint der Text "Hallo Android" und der Button verschwindet.

--- app/src/main/java/com/example/halloandroid/MainActivity.java
+++ app/src/main/java/com/example/halloandroid/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.halloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,7 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView view;
     private Button button;
-    private int i=0;
+    private int i = 100;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +22,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        i++;
-        String string = getResources().getString(R.string.newLayerText);
+        i--;
+        String string;
+        if (i == 0) {
+            string = getResources().getString(R.string.layerTextResult);
+            button.setVisibility(Button.INVISIBLE);
+        } else {
+            string = getResources().getString(R.string.layerText);
+        }
         view.setText(String.format(string, i));
+
     }
 }
